4a8b4dcd207fff1da9463a48dd51e37ed5df9ea8
Hamuj12 <hamza.mujtaba@utexas.edu>
2024-04-20 00:32:00 -0500

    Initial Commit

---
+++ VisionLLM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VisionLLM
+//
+//  Created by Hamza Mujtaba on 4/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisionLLM/VisionLLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionLLMApp.swift
+//  VisionLLM
+//
+//  Created by Hamza Mujtaba on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionLLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
